c6e457e724ea01f61bc201625c16ce1b097d283d
Omar Zapata <Omar.Zapata@cern.ch>
2018-03-30 13:41:00 -0500

    added more cmssw modules

--- env.sh
+++ env.sh
@@ -5,11 +5,10 @@ CMSSW=CMSSW_10_1_0
 SCRAM=slc6_amd64_gcc630
 GITUSER=omazapa
 
-#export
-export SCRAN_ARCH=$SCRAM
+#exports
+export SCRAM_ARCH=$SCRAM
 
 #setting up  cmssw user for github developements
-
 git config --global user.github $GITUSER
 
 
@@ -20,11 +19,56 @@ cmsenv
 git cms-init
 git clone git@github.com:HENGEX/GeneratorInterface.git
 
-#setting up Gen Production modules
-#https://twiki.cern.ch/twiki/bin/viewauth/CMS/GitRepositoryForGenProduction
+#setting up cmssw require modules modules
+git cms-addpkg  Configuration/Generator 
+git cms-addpkg  Configuration/EventContent
+git cms-addpkg  Configuration/StandardSequences
 
-mkdir -p Configuration/GenProduction/
-git clone git@github.com:cms-sw/genproductions.git Configuration/GenProduction/
+git cms-addpkg  SimGeneral/HepPDTESSource
+git cms-addpkg  SimGeneral/HepPDTRecord
+
+git cms-addpkg  GeneratorInterface/AlpgenInterface
+git cms-addpkg  GeneratorInterface/AMPTInterface
+git cms-addpkg  GeneratorInterface/BeamHaloGenerator
+git cms-addpkg  GeneratorInterface/CascadeInterface
+git cms-addpkg  GeneratorInterface/Configuration
+git cms-addpkg  GeneratorInterface/Core
+git cms-addpkg  GeneratorInterface/CosmicMuonGenerator
+git cms-addpkg  GeneratorInterface/EvtGenInterface
+git cms-addpkg  GeneratorInterface/ExhumeInterface
+git cms-addpkg  GeneratorInterface/ExternalDecays
+git cms-addpkg  GeneratorInterface/GenExtensions
+git cms-addpkg  GeneratorInterface/Herwig6Interface
+git cms-addpkg  GeneratorInterface/Herwig7Interface
+git cms-addpkg  GeneratorInterface/HiGenCommon
+git cms-addpkg  GeneratorInterface/HijingInterface
+git cms-addpkg  GeneratorInterface/Hydjet2Interface
+git cms-addpkg  GeneratorInterface/HydjetInterface
+git cms-addpkg  GeneratorInterface/LHEInterface
+git cms-addpkg  GeneratorInterface/MCatNLOInterface
+git cms-addpkg  GeneratorInterface/PartonShowerVeto
+git cms-addpkg  GeneratorInterface/PhotosInterface
+git cms-addpkg  GeneratorInterface/PomwigInterface
+git cms-addpkg  GeneratorInterface/PyquenInterface
+git cms-addpkg  GeneratorInterface/Pythia6Interface
+git cms-addpkg  GeneratorInterface/Pythia8Interface
+git cms-addpkg  GeneratorInterface/ReggeGribovPartonMCInterface
+git cms-addpkg  GeneratorInterface/RivetInterface
+git cms-addpkg  GeneratorInterface/SherpaInterface
+git cms-addpkg  GeneratorInterface/TauolaInterface
+
+
+git cms-addpkg  SimDataFormats/GeneratorProducts
+
+#GeomBuilder has a bug, xml file dont exists
+#edm::FileInPath unable to find file MagneticField/GeomBuilder/data/MagneticFieldVolumes_1_v7_large.xml anywhere in the search path.
+#I fix it taking it from old cvs code.
+
+git cms-addpkg MagneticField/GeomBuilder/
+cd MagneticField/GeomBuilder/data/
+wget https://raw.githubusercontent.com/cms-cvs-history/MagneticField-GeomBuilder/master/data/MagneticFieldVolumes_1_v7_large.xml
+wget https://raw.githubusercontent.com/cms-cvs-history/MagneticField-GeomBuilder/master/data/MagneticFieldVolumes_2_v7_large.xml
+cd ../../../
 
 #init voms
 voms-proxy-init --voms cms --valid 168:00
